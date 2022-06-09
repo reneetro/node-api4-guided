6731edc8ae8d72b39cdcccd8bd5a6e13b302156b
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-06-09 11:52:01 -0600

    fixed title

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('<h1> Hello World/h1>')
+    res.send('<h1> Hello World</h1>')
 })
 
 module.exports = server;
