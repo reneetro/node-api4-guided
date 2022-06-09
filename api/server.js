8a6433df4274d193b21e2618cd6a122c84cc4179
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-06-09 11:45:59 -0600

    demo push

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('<h1> Hello World/h1>')
+})
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,23 @@
+// function someProcedure(a){
+//     if(a === 1){
+//         return 5;
+//     } else if (a === 2){
+//         return 10;
+//     }
+
+//     throw new Error("a should be 1 or 2");
+// }
+
+
+// try {
+//     console.log('my result is:', someProcedure(1))
+// } catch(error){
+//     console.log('I got an exception', error.message);
+// }
+require('dotenv').config();
+
+const server = require('./api/server');
+
+server.listen(process.env.PORT || 9000 , () => {
+    console.log('server is running')
+})
